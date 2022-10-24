26e4bf5634de35ee2711496b010bfc2ec88db0f7
DSC <petrgame21@gmail.com>
2022-10-24 22:31:09 +0200

    Prvni upravy

---
+++ src/components/default/header.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+
+const Header = () => {
+    return (
+        <header>
+            <h1 class="title">DSC portfolio</h1>
+            <span class="subtitle">Web developer</span>
+        </header>
+    )
+}
+
+export default Header
