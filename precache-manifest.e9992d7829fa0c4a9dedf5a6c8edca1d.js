dc469810a291202028fabbdbef24a6bdbd043868
DSC <petrgame21@gmail.com>
2023-01-23 15:45:02 +0100

    Updates

---
+++ precache-manifest.e9992d7829fa0c4a9dedf5a6c8edca1d.js
@@ -0,0 +1,78 @@
+self.__precacheManifest = [
+  {
+    "revision": "c463fde3087b8c169229",
+    "url": "/personal/static/css/main.deecdda1.chunk.css"
+  },
+  {
+    "revision": "c463fde3087b8c169229",
+    "url": "/personal/static/js/main.210bbed8.chunk.js"
+  },
+  {
+    "revision": "28e4a844449b63ec867d",
+    "url": "/personal/static/js/runtime~main.1ce33e60.js"
+  },
+  {
+    "revision": "b297f2c809ed7c69af09",
+    "url": "/personal/static/css/2.66e38528.chunk.css"
+  },
+  {
+    "revision": "b297f2c809ed7c69af09",
+    "url": "/personal/static/js/2.f6d6182a.chunk.js"
+  },
+  {
+    "revision": "a47782186af4b287499d962a6f65cb1d",
+    "url": "/personal/static/media/sfkkozel.a4778218.png"
+  },
+  {
+    "revision": "92c5c0efca5c3252cf024c318db3780f",
+    "url": "/personal/static/media/mineleague.92c5c0ef.png"
+  },
+  {
+    "revision": "5eefd001a5e9c0ba6f489cb0ac74ce1e",
+    "url": "/personal/static/media/vue.5eefd001.png"
+  },
+  {
+    "revision": "5d2fa346caff80642b6b695964815b1e",
+    "url": "/personal/static/media/discord.5d2fa346.jpg"
+  },
+  {
+    "revision": "97ddc0cbfb465d346fc12ee87e0b79a2",
+    "url": "/personal/static/media/html.97ddc0cb.jpg"
+  },
+  {
+    "revision": "17ea81a7d7599f1c65deca1e550bd790",
+    "url": "/personal/static/media/js.17ea81a7.jpg"
+  },
+  {
+    "revision": "569b5a5e5e758c31421f1a2a973aa075",
+    "url": "/personal/static/media/php.569b5a5e.jpg"
+  },
+  {
+    "revision": "dfaff328c9a09927cb789514b207201a",
+    "url": "/personal/static/media/programator.dfaff328.jpg"
+  },
+  {
+    "revision": "8af7f62038f0de138ff91ba240e2a5ed",
+    "url": "/personal/static/media/nekurackaspolecnost.8af7f620.png"
+  },
+  {
+    "revision": "48a955f7292656fc2f570dcc9496d6ea",
+    "url": "/personal/static/media/uveryhypoteky.48a955f7.png"
+  },
+  {
+    "revision": "887d6cb716b6debd2413f482d6bcf175",
+    "url": "/personal/static/media/lexten.887d6cb7.png"
+  },
+  {
+    "revision": "314ee319e808bbc19315327758e30639",
+    "url": "/personal/static/media/srp.314ee319.png"
+  },
+  {
+    "revision": "9738c40c138c6f799729b5aecd9287df",
+    "url": "/personal/static/media/tagbroker.9738c40c.png"
+  },
+  {
+    "revision": "c1299ec1376503c0ade0e25dd3537376",
+    "url": "/personal/index.html"
+  }
+];
